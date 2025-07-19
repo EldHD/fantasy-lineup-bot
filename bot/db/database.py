b355bbb9740894d742372cbd3b0b64b8a974b74a
EldHD <eldar.hay@gmail.com>
2025-07-19 18:11:47 +0200

    Update database.py

--- bot/db/database.py
+++ bot/db/database.py
@@ -1,7 +1,9 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-DATABASE_URL = os.environ["DATABASE_URL"]  # формат: postgresql+asyncpg://...
+# Ожидается переменная окружения DATABASE_URL формата:
+# postgresql+asyncpg://USER:PASSWORD@HOST:PORT/DBNAME
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_async_engine(
     DATABASE_URL,
