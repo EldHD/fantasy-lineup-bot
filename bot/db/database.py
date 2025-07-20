ec40abd14c08194beb7d4a977a4ab9b267070543
EldHD <eldar.hay@gmail.com>
2025-07-20 19:17:26 +0200

    Update database.py

--- bot/db/database.py
+++ bot/db/database.py
@@ -1,9 +1,14 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from bot.config import DATABASE_URL
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL not set")
+from bot.db.models import Base
 
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def create_all():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def drop_all():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
